b3e1d5c8b452730b5d1b3e7a104975b6467279a9
ella <randomgive98@gmail.com>
2026-03-17 20:07:50 +0100

    05

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,8 @@
 # Check the total amount in the wallet.
+if ! bitcoin-cli -regtest listwallets | grep -q "builderswallet"; then
+    bitcoin-cli -regtest createwallet "builderswallet" > /dev/null
+fi
+bitcoin-cli -regtest loadwallet "builderswallet" > /dev/null 2>&1 || true
+ADDRESS=$(bitcoin-cli -regtest getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS" > /dev/null 2>&1
 bitcoin-cli -regtest getbalance
