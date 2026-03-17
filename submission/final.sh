f0eb21f1ee366f591fca1dffd48ade4e14974325
ella <randomgive98@gmail.com>
2026-03-18 00:53:04 +0100

    final last

--- submission/final.sh
+++ submission/final.sh
@@ -176,7 +176,7 @@ check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.pubkey')
+INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.desc' | sed -n 's/.*tr(\([^)]*\)).*/\1/p')
 check_cmd "Extracting key from descriptor"
 INTERNAL_KEY=$(trim "$INTERNAL_KEY")
 
