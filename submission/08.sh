33cff660c06011dd4c293943557e6b8f7dadba9a
ella <randomgive98@gmail.com>
2026-03-17 20:34:24 +0100

    06-8

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,3 @@
 # Generate a native SegWit address.
+bitcoin-cli -regtest loadwallet "testwallet" 2>/dev/null || bitcoin-cli -regtest createwallet "testwallet" > /dev/null
 bitcoin-cli -regtest -rpcwallet="testwallet" getnewaddress "" bech32
